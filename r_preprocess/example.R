176cc869f08f87546f72dbf185bd267daa66290e
田苗苗 TianMiaomiao <tmm20010925@163.com>
2026-09-11 09:42:02 +0800

    Fix species name format in example.R

--- r_preprocess/example.R
+++ r_preprocess/example.R
@@ -14,7 +14,7 @@ seurat2mmmvae(
 
 seurat2mmmvae(
   sample = "single-cell_exampleDATA",
-  species = "Homo sapiens",
+  species = "Homo_sapiens",
   datatype = "single_cell",
   root = "./example/single-cell_exampleDATA/",
   output_root = "./example/single-cell_exampleDATA/mmmvaeinput"
